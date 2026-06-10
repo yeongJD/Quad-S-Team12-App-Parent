0ff8d136b82f9c60b3fc60f8da595448707b22db
yeongJD <pyeon136@gmail.com>
2026-06-11 08:07:40 +0900

    style: align parent design tokens

--- lib/core/theme/app_tokens.dart
+++ lib/core/theme/app_tokens.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+
 abstract final class AppTokens {
   static const double mobileFrameWidth = 375;
   static const double mobileCanvasHeight = 812;
@@ -10,6 +12,23 @@ abstract final class AppTokens {
   static const double itemGap = 16;
   static const double smallGap = 8;
   static const double mediumGap = 12;
+  static const double photoGap = 12;
+
+  // Radii
   static const double cardRadius = 28;
+  static const double cardRadiusSmall = 16;
+  static const double dialogRadius = 12;
+  static const double fieldRadius = 12;
+  static const double buttonRadius = 8;
+  static const double bottomSheetTopRadius = 24;
+  static const double errorBannerRadius = 8;
+
   static const double cardPadding = 24;
+
+  // Component sizing
+  static const double bottomSheetHeight = 397.0;
+  static const double topBarHeight = 52;
+
+  // Shadows
+  static const Color cardShadowColor = Color(0x80D9D9D9);
 }
